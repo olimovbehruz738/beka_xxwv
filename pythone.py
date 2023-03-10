db96bc8d772817298e47dfb10aa632178741ecac
olimovbehruz738 <127301246+olimovbehruz738@users.noreply.github.com>
2023-03-10 16:35:51 +0500

    Update pythone.py

---
+++ pythone.py
@@ -0,0 +1,11 @@
+ismi=input("ismingiz nima")
+familyasi= input("familyagiz nima")
+login=input("Eliktron pochta nima")
+parol=input("codingiz nima")
+
+print ( f"" "mening ismim {ismim} {familyagiz}. mening yoshimda {2008} da! E-pochta esa manzilim: {email}, uning paroliesa ****{parol[4:]****" )
+text = "Mexanizatsiyalashtirishmoqchiliginiaytganinisezibqoldimmikana"
+ 
+print(text[0:13])
+print(text[31:33]) + text[13:15]
+
